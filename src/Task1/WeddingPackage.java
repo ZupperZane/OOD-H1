a4bf08a71f0bb8a279838c78a10f8802a6f7408d
HAJ02 <Haj020206@gmail.com>
2025-09-30 09:16:08 -0400

    Intial task 1 Structure

---
+++ src/Task1/WeddingPackage.java
@@ -0,0 +1,8 @@
+package Task1;
+
+public abstract class WeddingPackage {
+    String venue;
+    String catering;
+    String decorations;
+    String photography;
+}
---
+++ src/Task1/weddingFactory.java
@@ -0,0 +1,13 @@
+package Task1;
+
+public class weddingFactory {
+    void CreatePackage(String PackageType){
+        if (PackageType == "Luxury"){
+
+        } else if (PackageType == "Traditonal") {
+
+        } else if (PackageType == "Destination"){
+
+        }
+    }
+}
