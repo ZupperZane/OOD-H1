288695649559f16e10f92d82c6dc968c2731ee6c
HAJ02 <Haj020206@gmail.com>
2025-10-06 13:51:54 -0400

    Package Builder done?

--- src/Task1/weddingPackageBuilder.java
+++ src/Task1/weddingPackageBuilder.java
@@ -1,5 +1,14 @@
 package Task1;
 
 public class weddingPackageBuilder {
+    WeddingPackage Temp;
 
+    public buildPackage(String ven,String cater,String deco, String photo){
+        temp =new WeddingPackage;
+
+        temp.Destination = ven;
+        temp.catering=cater;
+        temp.decorations=deco;
+        temp.photography=photo;
+    }
 }
