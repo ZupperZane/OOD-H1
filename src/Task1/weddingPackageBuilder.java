6ede195cc233455edbc85244c719db23416b410d
HAJ02 <Haj020206@gmail.com>
2025-10-06 11:40:05 -0400

    Wedding Planner Singleton implemented, Package builder begun

--- src/Task1/weddingPackageBuilder.java
+++ src/Task1/weddingPackageBuilder.java
@@ -1,4 +1,5 @@
 package Task1;
 
 public class weddingPackageBuilder {
+
 }
--- src/Task1/weddingPlanner.java
+++ src/Task1/weddingPlanner.java
@@ -1,4 +1,23 @@
 package Task1;
 
 public class weddingPlanner {
+
+    private static weddingPlanner instance =null;
+
+    private String testcase;
+
+    private WeddingPlanner(){
+        testcase = "success"
+    }
+
+    public static weddingPlanner getInstance() {
+        if (instance == null){
+            synchronized (weddingPlanner.class){
+                if (instance == null) {
+                    instance = new weddingPlanner();
+                }
+            }
+        }
+        return instance;
+    }
 }
